db230d042f3a1e01fc9b4d37993b433970ee4be8
tomcorey26 <tombcorey@gmail.com>
2025-01-26 17:14:23 -0500

    Remove ESLint config and update dashboard layout styling

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,18 +1,16 @@
-"use client";
-import { useState } from "react";
+'use client';
+import { useState } from 'react';
 
 export default function FlashCardsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  // const [isDarkMode, setIsDarkMode] = useState(true);
 
   return (
     <div
-      className={`grid grid-rows-[auto_1fr_auto] min-h-screen p-8 gap-8 ${
-        isDarkMode ? "bg-gray-900 text-gray-200" : "bg-gray-100 text-gray-800"
-      }`}
+      className={`grid grid-rows-[auto_1fr_auto] min-h-screen p-8 gap-8 bg-gray-100 text-gray-800`}
     >
       {children}
     </div>
