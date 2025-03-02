a65f4cd6d56f4e8667a0bd6873dd415519d2a139
tomcorey26 <tombcorey@gmail.com>
2025-03-02 18:22:44 -0500

    Deleteing user working

--- src/server/db/decks.ts
+++ src/server/db/decks.ts
@@ -1,23 +1,8 @@
 import { db } from "@/drizzle/db";
 import { DeckTable } from "@/drizzle/schema";
 
-export async function createDeck({
-  name,
-  clerkUserId,
-  description,
-  imageUrl,
-  languages,
-}: typeof DeckTable.$inferInsert) {
-  const [deck] = await db
-    .insert(DeckTable)
-    .values({
-      name,
-      clerkUserId,
-      description,
-      imageUrl,
-      languages,
-    })
-    .returning();
+export async function createDeck(data: typeof DeckTable.$inferInsert) {
+  const [deck] = await db.insert(DeckTable).values(data).returning();
 
   return deck;
 }
